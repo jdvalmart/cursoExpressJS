308d33c928927152359c772d2abfd306bf461d1f
jdvalmart <juanvalencia9411@outlook.com>
2025-02-25 21:49:25 -0500

    vercer1.2

--- api/index.js
+++ api/index.js
@@ -16,16 +16,15 @@ const options = {
     if (whitelist.includes(origin) || !origin){
       callback(null, true)
     } else {
-    callback(new Error('no permitido'))
+      callback(new Error('no permitido'))
     }
   }
 }
 app.use(cors(options))
 
-app.get('/', (req, res)=>{
-  res.send('Mi servidor en express')
+app.get('/api', (req, res) =>{
+  res.send('corriendo en mi servidor')
 })
-
 
 routerApi(app)
 
